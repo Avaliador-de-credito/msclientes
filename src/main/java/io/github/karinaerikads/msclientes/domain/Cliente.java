d22e15c58f6c0088c0a576ec16faf9385eb34ebe
karinaerikads <karinaerikads48@gmail.com>
2023-12-29 12:03:13 -0300

    feat: adiciona domínio cliente

---
+++ src/main/java/io/github/karinaerikads/msclientes/domain/Cliente.java
@@ -0,0 +1,29 @@
+package io.github.karinaerikads.msclientes.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String cpf;
+    @Column
+    private String nome;
+    @Column
+    private Integer idade;
+
+    public Cliente(String cpf, String nome, Integer idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
